415f76b948045ef29adde231ae2615247de5b877
Dori <qarkchanjin@gmail.com>
2024-05-10 15:36:17 +0900

    KakaoAPI 삭제 [사유: 사용 안함] MemberController 부분 카카오 로그인 단순해짐. 인증, 콜백 처리는 프론트에서 하기로

--- src/main/java/awpterm/backend/controller/MemberController.java
+++ src/main/java/awpterm/backend/controller/MemberController.java
@@ -1,7 +1,5 @@
 package awpterm.backend.controller;
 
-import awpterm.backend.Config;
-import awpterm.backend.api.kakao.KakaoAPI;
 import awpterm.backend.api.request.MemberLoginRequestDTO;
 import awpterm.backend.api.request.MemberRegisterRequestDTO;
 import awpterm.backend.api.response.ApiResponse;
@@ -10,12 +8,10 @@ import awpterm.backend.enums.Gender;
 import awpterm.backend.enums.Major;
 import awpterm.backend.enums.Position;
 import awpterm.backend.service.MemberService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -54,46 +50,22 @@ public class MemberController {
         return ApiResponse.response(HttpStatus.CREATED, memberService.register(member));
     }
 
-    @GetMapping("/kakao/auth")
-    public ApiResponse<String> kakaoAuth() {
-        return ApiResponse.response(HttpStatus.OK, KakaoAPI.requestAuthorize().toString());
-    }
 
-    @GetMapping("/kakao/callback")
-    public void kakaoCallBack(String code, HttpServletResponse res) throws IOException {
-        res.sendRedirect(Config.SERVER_URL + "/member/kakao/login?id="+code+"&password="+KakaoAPI.requestToken(code));
-    }
-
-    @GetMapping("/kakao/login")
-    public ApiResponse<String> kakaoLogin(@RequestParam String clientId, @RequestParam String token) {
-        Member member = Member.builder()
-                .id(clientId)
-                .password(token.hashCode())
-                .build();
-
-        if (!memberService.login(member)) {
-            memberService.register(member); //아이디와 패스워드만 가진 녀석 DB에 저장
-            return ApiResponse.response(HttpStatus.CREATED, clientId);
+    @PostMapping("/kakao/login")
+    public ApiResponse<Boolean> kakaoLogin(@RequestBody MemberRegisterRequestDTO memberLoginRequestDTO) {
+        Member findMember = memberService.findById(memberLoginRequestDTO.getId());
+        if (findMember == null) {
+            return ApiResponse.response(HttpStatus.UNPROCESSABLE_ENTITY, "카카오 회원이 현재 존재하지 않습니다.", Boolean.FALSE);
         }
 
-        return ApiResponse.response(HttpStatus.OK, clientId);
-    }
-
-    @PostMapping("/kakao/register")
-    public ApiResponse<List<Member>> kakaoRegister(@RequestBody MemberRegisterRequestDTO memberRegisterRequestDTO) { //자동으로 회원가입이 되게끔
-        Member findMember = memberService.findById(memberRegisterRequestDTO.getId());
         Member member = Member.builder()
-                .id(findMember.getId())
-                .password(findMember.getPassword())
-                .name(memberRegisterRequestDTO.getName())
-                .birthDate(memberRegisterRequestDTO.getBirthDate())
-                .code(memberRegisterRequestDTO.getCode())
-                .phoneNumber(memberRegisterRequestDTO.getPhoneNumber())
-                .email(memberRegisterRequestDTO.getEmail())
-                .gender(Gender.valueOf(memberRegisterRequestDTO.getGender()))
-                .major(Major.valueOf(memberRegisterRequestDTO.getMajor()))
-                .position(Position.valueOf(memberRegisterRequestDTO.getPosition()))
+                .id(memberLoginRequestDTO.getId())
+                .password(memberLoginRequestDTO.getPassword().hashCode())
                 .build();
-        return ApiResponse.response(HttpStatus.CREATED, memberService.register(member));
+
+        if (!memberService.login(member))
+            return ApiResponse.response(HttpStatus.BAD_REQUEST, "아이디 혹은 비밀번호가 일치하지 않습니다.", Boolean.FALSE);
+
+        return ApiResponse.response(HttpStatus.OK, Boolean.TRUE);
     }
 }
