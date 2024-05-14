9d50a7b1a6bbcefdd9441de35cfe15b4c081e85b
wintee-laptop <b12e34e56@naver.com>
2024-05-14 15:05:01 +0900

    ApiResponse 추가 수정

--- src/main/java/awpterm/backend/api/response/ApiResponse.java
+++ src/main/java/awpterm/backend/api/response/ApiResponse.java
@@ -7,13 +7,8 @@ import org.springframework.http.ResponseEntity;
 
 @Data
 @Builder
-public class ApiResponse<T>{
-    private T data;
-
-    public static <T> ResponseEntity<ApiResponse<T>> response(HttpStatus code, T data) {
-        return new ResponseEntity<>(
-                ApiResponse.<T>builder()
-                .data(data)
-                .build(), code);
+public class ApiResponse{
+    public static <T> ResponseEntity<T> response(HttpStatus code, T data) {
+        return new ResponseEntity<>(data, code);
     }
 }
